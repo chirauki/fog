60b3d2d9d9aa3739a6c7a30a11d304fc7dc480cd
sergiopena <sergio.pena@abiquo.com>
2014-06-19 18:34:31 +0200

    Added lnk generation to datacenter

--- lib/fog/abiquo/models/compute/datacenter.rb
+++ lib/fog/abiquo/models/compute/datacenter.rb
@@ -7,11 +7,39 @@ module Fog
         attribute :location
         attribute :name
         attribute :uuid
+        attribute :url
+        attribute :remoteservices_lnk
+        attribute :backups_lnk
+        attribute :updateusedresources_lnk
+        attribute :hypervisors_lnk
+        attribute :enterprises_lnk
+        attribute :discover_lnk
+        attribute :checkmachinestate_lnk
+        attribute :checkmachineipmistate_lnk
+        attribute :tiers_lnk
+        attribute :devices_lnk
+        attribute :networkservicetypes_lnk
+        attribute :network_lnk 
+        attribute :getLimits_lnk
 
         attribute :links
 
         def racks
           @racks ||= Fog::Compute::Abiquo::Racks.new :racks_lnk => self.links.select {|l| l['rel'] == 'racks' }.first['href'] , :service => service
+        end
+
+        def initialize(attributes)
+          prepare_service_value(attributes)
+
+          attributes['links'].each do |link|
+            rel = "#{link['rel'].gsub(/\//, '_')}_lnk"
+            if 'edit'.eql?(link['rel'])
+              rel = 'url'
+            end
+            attributes[rel] = link
+          end
+          
+          super
         end
 
       end # Datacenter
