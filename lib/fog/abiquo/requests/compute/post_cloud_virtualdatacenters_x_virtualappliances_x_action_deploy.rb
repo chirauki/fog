c3e2377bc1c0e21a9ea8333e883de0f9c1504e03
Marc Cirauqui <marc.cirauqui@abiquo.com>
2014-10-22 23:57:29 +0200

    Fixed deploy request for vapp

--- lib/fog/abiquo/requests/compute/post_cloud_virtualdatacenters_x_virtualappliances_x_action_deploy.rb
+++ lib/fog/abiquo/requests/compute/post_cloud_virtualdatacenters_x_virtualappliances_x_action_deploy.rb
@@ -2,21 +2,21 @@ module Fog
   module Compute
     class Abiquo
       class Real
-        def post_cloud_virtualdatacenters_x_virtualappliances_x_action_deploy(virtualdatacenter, virtualappliance, payload, options = {})
+        def post_cloud_virtualdatacenters_x_virtualappliances_x_action_deploy(virtualdatacenter, virtualappliance, options = {})
           request(
-            :expects  => [200, 201],
+            :expects  => [202],
             :method   => 'POST',
             :path     => "/cloud/virtualdatacenters/#{virtualdatacenter}/virtualappliances/#{virtualappliance}/action/deploy",
             :accept   => "application/vnd.abiquo.acceptedrequest+json",
             :content  => "application/vnd.abiquo.virtualmachinetask+json",
-            :body     => payload,
+            :body     => "{}",
             :query    => options
           )
         end
       end
 
       class Mock
-        def post_cloud_virtualdatacenters_x_virtualappliances_x_action_deploy(virtualdatacenter, virtualappliance, payload, options = {})
+        def post_cloud_virtualdatacenters_x_virtualappliances_x_action_deploy(virtualdatacenter, virtualappliance, options = {})
           Fog::Mock.not_implemented
         end
       end
